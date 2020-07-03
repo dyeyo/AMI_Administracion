70b053d5178756297449c476fffd9dac54bf170b
dyego vallejo <dyegovallejob@gmail.com>
2020-07-03 16:01:00 -0500

    s

--- resources/views/emails/EmailRegistro2.blade.php
+++ resources/views/emails/EmailRegistro2.blade.php
@@ -29,7 +29,6 @@
     <div class="contenedor" style="width: 650px; background: #fff; margin: auto;">
       <h3 style="text-align: center;">{{ $student['nombre'] }} Gracias por elegirnos.</h3>
       <img style='display: block; width: 100%; padding-bottom: 40px;' src='{{ asset('assets/img/01.png') }}'>
-      <img style='display: block; width: 100%; padding-bottom: 40px;' src='{{ asset('assets/img/02.png') }}'>
       <p>{{$item->firstText}}</p>
       <img src="{{asset('images/'.$item->image)}}" style="width: 50%" alt="">
       <h3 style="text-align: center;">Proceso de Matrícula</h3>
